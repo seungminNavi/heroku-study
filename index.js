9b3aa2e4e61ad1975a55f753cbf1675a3714d3e1
seungminNavi <101856112+seungminNavi@users.noreply.github.com>
2022-04-19 15:26:08 +0900

    asd

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM public.test_table');
       const results = { 'results' : (result) ? result.rows : null};
       res.render('pages/db', results);
       client.release();
